d654f5140990bc2cd0fde3fc2731312a95a70df4
Phan Van Da <phanvandauet1997@gmail.com>
2018-07-26 15:09:32 +0700

    init project

---
+++ MarvelMovie/MarvelMovie/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MarvelMovie
+//
+//  Created by Da on 7/26/18.
+//  Copyright © 2018 MarvelMovie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
