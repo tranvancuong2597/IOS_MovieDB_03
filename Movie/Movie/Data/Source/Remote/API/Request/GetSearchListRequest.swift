dae34d9a7614b3e1d02d75967a54cf93e376fa6a
Tran Van Cuong <tran.van.cuongb@framgia.com>
2018-08-06 20:10:04 +0700

    Search Screen

---
+++ Movie/Movie/Data/Source/Remote/API/Request/GetSearchListRequest.swift
@@ -0,0 +1,23 @@
+//
+//  GetSearchListRequest.swift
+//  Movie
+//
+//  Created by TranCuong on 8/4/18.
+//  Copyright © 2018 Tran Cuong. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+import Alamofire
+
+class GetSearchListRequest: BaseRequest {
+    required init(query: String, page: Int) {
+        let body: [String: Any]  = [
+            "api_key": APIKey.key,
+            "language": "en-US",
+            "query": query
+        ]
+        let url = URLs.apiMovieSearch
+        super.init(url: url, requestType: .get, body: body)
+    }
+}
---
+++ Movie/Movie/Data/Source/Remote/API/Respone/SearchMoviesResponse.swift
@@ -0,0 +1,26 @@
+//
+//  SearchMoviesResponse.swift
+//  Movie
+//
+//  Created by TranCuong on 8/4/18.
+//  Copyright © 2018 Tran Cuong. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class SearchMoviesResponse: Mappable {
+    var page: Int?
+    var totalPages: Int?
+    var movies = [Movie]()
+    
+    required init(map: Map) {
+        mapping(map: map)
+    }
+    
+    func mapping(map: Map) {
+        movies <- map["results"]
+        page <- map["page"]
+        totalPages <- map["total_pages"]
+    }
+}
